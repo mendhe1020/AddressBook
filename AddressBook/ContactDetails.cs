92dd6014b1eda30bc89c552e222dc735c96a3a82
mendhe1020 <anurag.mendhe14@gmail.com>
2022-10-05 15:47:25 +0530

    [Anurag] Add: Editing

--- AddressBook/ContactDetails.cs
+++ AddressBook/ContactDetails.cs
@@ -43,5 +43,65 @@ namespace AddressBook
                 Console.WriteLine("Phone Number of person : " + contact.PhoneNumber);
             }
         }
+        public void Edit()
+        {
+            Console.WriteLine("Enter the first name to search person details : ");
+            string name = Console.ReadLine();
+
+            foreach (Contact contacts1 in People)
+            {
+                if (contacts1.FirstName == name)
+                {
+                    Console.WriteLine("choose the option to change the details : \n1) First Name\n2) Last Name\n3) Address\n4) City\n5) State\n6) Zip\n7) Email\n8) Phone Number");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Please enter the First name : ");
+                            string FirstName = Console.ReadLine();
+                            contacts1.FirstName = FirstName;
+                            break;
+                        case 2:
+                            Console.WriteLine("Please enter the Last name : ");
+                            string LastName = Console.ReadLine();
+                            contacts1.LastName = LastName;
+                            break;
+                        case 3:
+                            Console.WriteLine("Please enter the Address : ");
+                            string Address = Console.ReadLine();
+                            contacts1.Address = Address;
+                            break;
+                        case 4:
+                            Console.WriteLine("Please enter the City : ");
+                            string City = Console.ReadLine();
+                            contacts1.City = City;
+                            break;
+                        case 5:
+                            Console.WriteLine("Please enter the State : ");
+                            string State = Console.ReadLine();
+                            contacts1.State = State;
+                            break;
+                        case 6:
+                            Console.WriteLine("Please enter the Zip Code : ");
+                            string Zip = Console.ReadLine();
+                            contacts1.Zip = Zip;
+                            break;
+                        case 7:
+                            Console.WriteLine("Please enter the Email : ");
+                            string Email = Console.ReadLine();
+                            contacts1.Email = Email;
+                            break;
+                        case 8:
+                            Console.WriteLine("Please enter the Phone Number : ");
+                            string PhoneNumber = Console.ReadLine();
+                            contacts1.PhoneNumber = PhoneNumber;
+                            break;
+                        default:
+                            Console.WriteLine("please choose correct options :");
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,15 +11,20 @@
 
             while (true)
             {
-                Console.WriteLine("Enter the option : \n1)Add Contact");
+                Console.WriteLine("Enter the option : \n1)Add Contact \n2)Edit Contact");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        contacts.AddingPerson(contacts1);
+                        contacts.AddingPerson(contacts1); 
+                        contacts.Display();
+                        break;
+                    case 2:
+                        contacts.Edit();        
                         contacts.Display();
                         break;
                     default:
+                        Console.WriteLine("please choose correct options :");
                         break;
                 }
             }
